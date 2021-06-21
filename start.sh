fa475a672f7f1180291bba31422e52d444c8d3d3
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-06-21 17:27:53 +0300

    Adding procfile and start.sh file to run the application

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MAIL_USERNAME='kiplangatdennis99@gmail.com'
+export MAIL_PASSWORD='kiplangat18'
+export SECRET_KEY='kiplaa'
+export DATABASE_URL='postgresql+psycopg2://moringa:kiplangat18@localhost/blogs'
+
+python3.8 manage.py server
